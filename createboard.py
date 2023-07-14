9e36f2245f239a69da7eb2c2747716c5b1cbc438
Sidney Johnson <sidneyj4750@gmail.com>
2023-07-14 10:10:56 -0400

    v2

--- createboard.py
+++ createboard.py
@@ -1,22 +1,43 @@
 from random import randint
 
+NO_USER_GUESS=10
+
 num = 5
 
 board = []
+
+def print_board(board):
+    print ("Let's Play Battleship!")
+print_board(board)
+
 # range() is columns
 for i in range(num):
     board.append(num * ["O"]) # this appends a list of five "O" strings
-#for z in board:
-    #print(*z)
-def print_board(board):
-    print ("Battleship")
-print_board(board)
+for z in board:
+    print(*z)
 
-# board done 
-guessRow = int(input("Guess Row:"))
-guessColumn = int(input("Guess Col:"))
+def random_row(board):
+    return randint(range(num) - 1)
 
-  
+def random_column(board):
+    return randint(range(num) - 1)
+
+shipRow = random_row(board)
+shipColumn = random_column(board)
+
+turn = 0
+for attempt in range(NO_USER_GUESS):
+    guessRow = int(input("Guess Row:"))
+    guessColumn = int(input("Guess Col:"))
+
+if guessRow == ship_row and guessColumn == ship_col:
+      print ("Hit")
+else:
+    if (guessRow < range(num) or guessRow > range(num)) or (guessColumn < range(num) or guessColumn > range(num)):
+          print ("Enter a Valid Number")
+    #elif(board[guessRow][guessColumn] == "X"):
+          #print ("You guessed that one already!!!")
+          
 #while board: 
 #        try:
  #           guessRow=(input("Enter a Row: "))
