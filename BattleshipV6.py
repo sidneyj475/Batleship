6ba96ffe3ae6182b3379c2cb6bb63a332a5c7c23
Sidney Johnson <sidneyj4750@gmail.com>
2023-07-19 12:51:34 -0400

    nice

--- BattleshipV6.py
+++ BattleshipV6.py
@@ -7,24 +7,25 @@ def print_board(board):
     for z in board:
         print(" ".join(z))
 
+def print_board2(board2):
+    for x in board2:
+        print(" ".join(x))
+
 '''We tried to allow for letting the user choose between random and deliberate placement,
 but we couldn't get it done in time and commented it out'''
 
 
 def makelo(board):
     #while True:
-        #try:
             #choice = (input("Would you like to place your ships?: y/n: "))
             
             #if choice == ("n"):
                 return random.randint(1, len(board)), random.randint(1, len(board[0]))
             
-            #else:
-                #inputrow=int(input("Row: "))
-                #inputcol=int(input("Column: "))
-        #except: 
-            #print("Whaaaaaa")
-
+            #elif choice == ("y"):
+            #    inputrow=int(input("Row: "))
+             #   inputcol=int(input("Column: "))
+            #break
 # Definining user input, input will be the shot that the player takes at the computer's board.
   
 def inputthatthang(grid):
@@ -45,7 +46,9 @@ print("Let's Play Battleship! Your enemy is on water. They're coming at you and 
 
 # User defines how large board will be. The board size will be the same for both players (user and computer)
 
-grid = int(input("Choose Your Grid Size:"))
+grid = int(input("Choose Your Grid Size: "))
+
+grid2 = grid
 
 # Board display is made
 
@@ -53,6 +56,9 @@ board = []
 for i in range(grid):
     board.append(["O"] * grid)
 
+board2 = []
+for o in range(grid2):
+    board2.append(["O"] * grid2)
 
 # Variable to represent rows and columns inside the board
 
@@ -68,36 +74,53 @@ for LAUNCH in range(6):
 # Stores user guesses in a list, output is result of user launch. 
     if LAUNCH == 5:
         print("Game Over.")
-        board[GuessRow][GuessColumn] = "X"
+        board2[GuessRow][GuessColumn] = "X"
         break
     
     print("Launch", LAUNCH + 1,"!")
-    print_board(board)
+    print_board2(board2)
     
     GuessRow, GuessColumn = inputthatthang(grid)
 
     if GuessRow + 1 == RowRowRow and GuessColumn + 1 == ColColCol:
         print("\nWow! You sunk an enemy ship!")
-        board[GuessRow][GuessColumn] = "X"
-        print_board(board)
-        print("")
+        board2[GuessRow][GuessColumn] = "X"
+        print_board2(board2)
+        print("Game Over!")
         break
     
     else:
-        if board[GuessRow][GuessColumn] == "-":
+        if board2[GuessRow][GuessColumn] == "-":
             print("\n YOU ALREADY GUESSED THAT :|.")
             print(" ")
         else:
             print("\n Come on man! You MISSED!")
-            board[GuessRow][GuessColumn] = "-"
-# Computer guess - If Computer Guess =  User's Inputted Board or User's Randomized Ship, the game ends.             
-    for compguess in range(1):
-        print("CPU guessed:",(cpuguess(board)))
+            board2[GuessRow][GuessColumn] = "-"
+            print(" ")
+
+# Computer guess - If Computer Guess =  User's Inputted Board or User's Randomized Ship, the game ends.    
+    for COMPGUESS in range(1):
+        print("CPU Turn:", COMPGUESS + 1,"!")
+        print_board(board)
+        
+        cguess = cpuguess(board)
+        
+        print("CPU guessed:",cguess)
         print(" ")
-        if cpuguess(board) == makelo(board):
+        
+
+        if cguess == makelo(board):
             print ("Your ship has been sunk! ;( ") 
             print("Game Over.")
+            board[cguess] = "X"
             break
+        #else: 
+            if board[cguess] == "-":
+                print(" ")
+            else:
+                print(" ")
+                board[cguess] = "-"
+                print(" ")
 
         
   
